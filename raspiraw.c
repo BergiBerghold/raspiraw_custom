ba6cbf2834ba80e15f300be7d585249fbd0e7d86
Michael Berghold <michael.berghold@oewf.org>
2023-10-04 21:47:13 -0700

    .

--- raspiraw.c
+++ raspiraw.c
@@ -2093,7 +2093,7 @@ int main(int argc, char **argv)
 	}
 
 	// Set up YUV/RGB processing outputs
-	yuv_pool = mmal_port_pool_create(isp->output[0], isp->output[0]->buffer_num, 0);
+	yuv_pool = 0; //mmal_port_pool_create(isp->output[0], isp->output[0]->buffer_num, 0);
 	if (!yuv_pool)
 	{
 		vcos_log_error("Failed to create yuv pool");
