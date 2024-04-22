53511c0343d03f8665aaf119ce043f76454075da
Michael Berghold <michael.berghold@oewf.org>
2024-04-22 16:21:06 +0200

    .

--- imx219_modes.h
+++ imx219_modes.h
@@ -105,6 +105,8 @@ struct sensor_regs imx219_8MPix[] =
     {0x4797, 0x0E},  // CIS Tuning ?
     {0x479B, 0x0E},  // CIS Tuning ?
     {0x0100, 0x01},  // Mode Select: Streaming
+    {0x0600, 0x00},
+    {0x0601, 0x07},
 };
 
 // Register setting provided on
